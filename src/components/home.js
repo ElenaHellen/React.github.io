9c45ee48a212a520e1bdc23eb0715d9497c414d7
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-08 12:43:53 +0200

    Update and rename Home.js to home.js

---
+++ src/components/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import {Link } from "react-router-dom";
+
+
+const Home = () => (
+ 
+      <div >
+
+        <div>Hello! It's Home Page</div>
+        <Link to = "/login">Login</Link>
+         
+      </div>
+    
+  );
+
+export default Home;
+
