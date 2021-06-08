46c941bc7270b9dc448a92a813c71bb096876f20
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-08 12:45:28 +0200

    Update map.js

--- src/components/map.js
+++ src/components/map.js
@@ -1,29 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Profile from "./profile";
+import {Link } from "react-router-dom";
 
-class Map extends React.Component{
-render() {
-  return (
+
+const Maps = () => (
  
-      <div className="App">
-        Map 
-        <Router>
-      <div className="App">
-        
-       <li> <Link to = "/profile">Profile</Link></li>
-       
-        <Switch>
-          
-          <Route exact path="/profile" component={Profile}></Route>
-         
-        </Switch>
-      </div>
-      </Router>
+      <div >
+        <div>Map </div>
+        <Link to="./home">Home</Link>
   
       </div>
     
   );
-}
-}
-export default Map;
+
+export default Maps;
+
