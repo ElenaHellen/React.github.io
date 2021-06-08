3ce565682d6cc3af6329b985aee7da5fbdafeb18
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-08 12:43:08 +0200

    Create AppRoutes.js

---
+++ src/AppRoutes.js
@@ -0,0 +1,7 @@
+export const root = "/home";
+export const about = "/about";
+export const contacts = "/contacts";
+export const profile = "/profile";
+export const geoSearch = "/geoSearch";
+export const login = "/login";
+export const map = "/map";
