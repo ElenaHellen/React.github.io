03980794f072813ef09dbc3d5ac23cd5caf1cf10
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-08 13:27:21 +0200

    N:\Elena\my-page\src
    new

---
+++ src/geoSearchContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GeoSearch from "../components/geoSearch/geoSearch";
+
+class GeoSerchs extends React.Component {
+  render() {
+    return (
+      <GeoSearch/>
+    );
+  }
+}
+
+export default GeoSerchs;
---
+++ src/mapContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Maps from "../components/map/map";
+
+class MapsPage extends React.Component {
+  render() {
+    return (
+      <Maps/>
+    );
+  }
+}
+
+export default MapsPage;
