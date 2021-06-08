d836d0316b1d9105fffab9b6f165c53cb078bb2d
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-08 13:24:16 +0200

    containers

---
+++ src/contactsContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Contacts from "../components/Contacts/contacts";
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <Contacts/>
+    );
+  }
+}
+
+export default Contact;
